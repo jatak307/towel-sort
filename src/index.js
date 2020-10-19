be99382d6968a6427d53010ad1c939dc2cc3bda6
Kota <jatak307@gmail.com>
2020-10-19 14:49:17 +0300

    feat: implement sorting function of multidimensional array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2) {
+            matrix[i].sort((a, b) => b - a)
+        } else {
+            matrix[i].sort((a, b) => a - b)
+        }
+      
+      for (let j = 0; j < matrix[i].length; j++) {
+        arr.push(matrix[i][j]);
+      }
+    }
+    return arr;
 }
